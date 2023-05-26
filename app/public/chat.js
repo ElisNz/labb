a52f2d89564c711435be4b254d6d8ea254dbcbe5
E.N <67209588+ElisNz@users.noreply.github.com>
2023-05-26 04:11:38 +0200

    removed comments

--- app/public/chat.js
+++ app/public/chat.js
@@ -3,24 +3,6 @@ const waitingRoom = "waiting room";
 let usersInChat = 0;
 var socket = io.connect('http://localhost:3000');
 
-/* const btnSend = document.querySelector('#send-button');
-const messageBox = document.querySelector('.message-box');
-
-btnSend.addEventListener('click', () => {
-  const input = document.querySelector('#message-input');
-  socket.emit('chat message', input.value)
-})
-
-socket.on('chat message', message => {
-  messageBox.innerHTML += `<div class="message">${message}</div>`
-})
-
-socket.on('server message', message => {
-  messageBox.innerHTML += `<div class="message">${message}</div>`
-}) */
-
-
-
 var message = document.getElementById('message');
     handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
